8ca0f41d1c71d66317c934291f89674863bbe4c1
sowmiyaramanathan <ramanathan.sowmiya02@gmail.com>
2025-01-27 11:29:04 +0530

    Changed the GetAccount to prevent Deadlock

--- db/query/account.sql
+++ db/query/account.sql
@@ -3,12 +3,16 @@ INSERT INTO accounts (
   owner, balance, currency
 ) VALUES (
   $1, $2, $3
-)
-RETURNING *;
+) RETURNING *;
 
 -- name: GetAccount :one
 SELECT * FROM accounts
 WHERE id = $1 LIMIT 1;
+
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1
+FOR NO KEY UPDATE;
 
 -- name: ListAccounts :many
 SELECT * FROM accounts
