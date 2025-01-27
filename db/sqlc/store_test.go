59b80433bc88fb3703f2762cd7e352aa100a4540
sowmiyaramanathan <ramanathan.sowmiya02@gmail.com>
2025-01-27 11:42:58 +0530

    Added a func to update balance

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -22,10 +22,8 @@ func TestTranferTx(t *testing.T) {
 	results := make(chan TranferTxResult)
 
 	for i := 0; i < n; i++ {
-		txName := fmt.Sprintf("tx %d", i+1)
 		go func() {
-			ctx := context.WithValue(context.Background(), txKey, txName)
-			result, err := store.TranferTx(ctx, TransferParams{
+			result, err := store.TranferTx(context.Background(), TransferParams{
 				FromAccountID: account1.ID,
 				ToAccountID:   account2.ID,
 				Amount:        amount,
